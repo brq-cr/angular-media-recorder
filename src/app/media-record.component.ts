0a8c4e3764d7ac61b3ea51897e68a1a2bfd3d100
brq-cr <b.rq@me.com>
2022-07-09 12:45:27 -0600

    Initial commit

---
+++ src/app/media-record.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'media-record',
+  template: `<h1>Hello</h1>`,
+  styles: [`h1 { font-family: Lato; }`],
+})
+export class MediaRecord {}
